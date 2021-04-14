eb5ac61e444eaa5835cb18a8c1ccf5b69b73d012
Daban Sadiq <dabansadiq@icloud.com>
2021-04-14 14:08:09 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form')
+  const ul = document.getElementById('tasks');
+  form.addEventListener('submit',addItem)
+
+  function addItem(e){
+
+  e.preventDefault()
+  const li =  document.createElement('li')
+  li.innerHTML = input.value
+  }
 });
