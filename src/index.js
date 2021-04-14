a63c795a041efc7be8809847fdf8cb7a6890a14c
Daban Sadiq <dabansadiq@icloud.com>
2021-04-14 14:09:50 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const form = document.getElementById('create-task-form')
   const ul = document.getElementById('tasks');
+  const input =document.getElementById('new-task-description');
   form.addEventListener('submit',addItem)
 
   function addItem(e){
